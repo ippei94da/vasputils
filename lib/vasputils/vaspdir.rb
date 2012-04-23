f7a59f9b70f472e3d00d2082ef152bab20a6e6f3
ippei94da <ippei94da@gmail.com>
2012-04-23 20:33:34 +0900

    small revision

--- lib/vasputils/vaspdir.rb
+++ lib/vasputils/vaspdir.rb
@@ -26,63 +26,35 @@ require "vasputils/kpoints.rb"
 # try00 形式の postfix がついていることを前提とする。
 # 00 の部分には CONTCAR を POSCAR にする手続きで連続して行う計算の番号を示す。
 #
-class VaspDir
-
+class VaspDir < Comana
   class InitializeError < Exception; end
-  class ConvergedError < Exception; end
-  class NotEndedError < Exception; end
-  class LockedError < Exception; end
-  class PostfixMismatchError < Exception; end
 
-  #POSTFIX = /try(\d+)$/ for geom opt
-
-  attr_reader :mode
-
-  # do nothing.
-  def prepare_next
+  # 配下の OUTCAR を Outcar インスタンスにして返す。
+  # 存在しなければ例外 Errno::ENOENT を返す。
+  def outcar
+    Outcar.load_file("#{@dir}/OUTCAR")
   end
 
-
-  # 正常に終了していれば true を返す。
-  # 実行する前や実行中、OUTCAR が完遂していなければ false。
-  #
-  # MEMO
-  # PI12345 ファイルは実行中のみ存在し、終了後 vasp (mpi？) に自動的に削除される。
-  def normal_ended?
-    begin
-      return Outcar.load_file("#{@dir}/OUTCAR")[:normal_ended]
-    rescue Errno::ENOENT
-      return false
-    end
+  # 配下の CONTCAR を Cell2 インスタンスにして返す。
+  # 存在しなければ例外 Errno::ENOENT を返す。
+  def contcar
+    Poscar.load_file("#{@dir}/CONTCAR")
   end
 
-  # normal_ended? が false なら false。
-  # normal_ended? が true のうち、
-  # 結果を使って次に計算すべきなら true を、そうでなければ false を返す。
+  # 配下の KPOINTS を読み込んだ結果をハッシュにして返す。
   #
-  # 計算すべき、の条件はモードによって異なる。
-  #   NSW = 0 もしくは NSW = 1 のとき、必ず false。
-  #   - :single_point     モードならば、常に false。
-  #   - :geom_opt_lattice モードならば、ionic step が 2 以上なら true。
-  #   - :geom_opt_atoms   モードならば、ionic step が NSW と同じなら true。
-  #
-  def to_be_continued?
-    begin
-      outcar = Outcar.load_file("#{@dir}/OUTCAR")
-    rescue Errno::ENOENT
-      return false
-    end
-    ionic_steps = outcar[:ionic_steps]
-    return false unless outcar[:normal_ended]
-    return false if @incar["NSW"].to_i <= 1
-    if @mode == :geom_opt_lattice
-      return ionic_steps != 1
-    elsif @mode == :geom_opt_atoms
-      return ionic_steps == @incar["NSW"].to_i
-    else
-      return false
-    end
+  # 存在しなければ例外 Errno::ENOENT を返す筈だが、
+  # vasp dir の判定を incar でやっているので置こる筈がない。
+  def incar
+    Incar.load_file("#{@dir}/INCAR")
   end
+
+  # 配下の KPOINTS を読み込んだ結果をハッシュにして返す。
+  def kpoints
+    Kpoints.load_file("#{@dir}/KPOINTS")
+  end
+
+  private
 
   # vasp を投げる。
   # 計算実行時に lock を生成する。
@@ -96,7 +68,7 @@ class VaspDir
   # machinefile を生成しないとどのホストで計算するか、安定しない。
   # そのうち mpiexec from torque に対応するが、
   # まずは mpirun で動くように作る。
-  def calculate
+  def send_command
     raise LockedError if started?
 
     File.open(lock_file, "w") do |lock_io|
@@ -152,75 +124,6 @@ class VaspDir
     end
   end
 
-  # 次の計算ディレクトリを作成し、
-  # その VaspDir クラスで self を置き換える。
-  # 計算が正常終了していなければ、例外 VaspDirNotEndedError を生じる。
-  # 次の計算ディレクトリが既に存在していれば例外 Errno::EEXIST が投げられる。
-  def next
-    raise NotEndedError unless normal_ended?
-    raise ConvergedError unless to_be_continued?
-    #postfix = /try(\d+)$/
-    POSTFIX =~ @dir
-    try_num = $1.to_i
-    next_dir = @dir.sub(POSTFIX, sprintf("try%02d", try_num + 1))
-    Dir.mkdir next_dir
-    FileUtils.cp( "#{@dir}/INCAR"  , "#{next_dir}/INCAR")
-    FileUtils.cp( "#{@dir}/KPOINTS", "#{next_dir}/KPOINTS")
-    FileUtils.cp( "#{@dir}/POTCAR" , "#{next_dir}/POTCAR")
-    FileUtils.cp( "#{@dir}/CONTCAR", "#{next_dir}/POSCAR")
-    initialize(next_dir)
-  end
-
-  # Postprocess.
-  def teardown
-    # Do nothing.
-  end
-
-  # Return number of electronic steps.
-  def internal_steps
-    return outcar[:electronic_steps] if outcar
-    return 0
-  end
-
-  # Return number of ionic steps.
-  def external_steps
-    return outcar[:ionic_steps] if outcar
-    return 0
-  end
-
-  # Return elapsed time.
-  def elapsed_time
-    return outcar[:elapsed_time] if outcar
-    return 0.0
-  end
-
-  # 配下の OUTCAR を Outcar インスタンスにして返す。
-  # 存在しなければ例外 Errno::ENOENT を返す。
-  def outcar
-    Outcar.load_file("#{@dir}/OUTCAR")
-  end
-
-  # 配下の CONTCAR を Cell2 インスタンスにして返す。
-  # 存在しなければ例外 Errno::ENOENT を返す。
-  def contcar
-    Poscar.load_file("#{@dir}/CONTCAR")
-  end
-
-  # 配下の KPOINTS を読み込んだ結果をハッシュにして返す。
-  #
-  # 存在しなければ例外 Errno::ENOENT を返す筈だが、
-  # vasp dir の判定を incar でやっているので置こる筈がない。
-  def incar
-    Incar.load_file("#{@dir}/INCAR")
-  end
-
-  # 配下の KPOINTS を読み込んだ結果をハッシュにして返す。
-  def kpoints
-    Kpoints.load_file("#{@dir}/KPOINTS")
-  end
-
-  private
-
   #INCAR 解析とかして、モードを調べる。
   #- 格子定数の構造最適化モード(ISIF = 3)
   #- 格子定数を固定した構造最適化モード(ISIF = 2)
@@ -248,7 +151,91 @@ class VaspDir
     else
         @mode = nil
     end
+
+    # e.g.,
+    #@logfile    = "comana.log"
+    #@alive_time = 3600
+    #@outfiles   = ["output_a", "ouput_b"] # Files only to output should be indicated.
+  end
+
+  # 正常に終了していれば true を返す。
+  # 実行する前や実行中、OUTCAR が完遂していなければ false。
+  #
+  # MEMO
+  # PI12345 ファイルは実行中のみ存在し、終了後 vasp (mpi？) に自動的に削除される。
+  def finished?
+    begin
+      return Outcar.load_file("#{@dir}/OUTCAR")[:normal_ended]
+    rescue Errno::ENOENT
+      return false
+    end
   end
 
 end
 
+#class VaspGeometryOptimization < Comana
+#  # 次の計算ディレクトリを作成し、
+#  # その VaspDir クラスで self を置き換える。
+#  # 計算が正常終了していなければ、例外 VaspDirNotEndedError を生じる。
+#  # 次の計算ディレクトリが既に存在していれば例外 Errno::EEXIST が投げられる。
+#  def next
+#    raise NotEndedError unless normal_ended?
+#    raise ConvergedError unless to_be_continued?
+#    #postfix = /try(\d+)$/
+#    POSTFIX =~ @dir
+#    try_num = $1.to_i
+#    next_dir = @dir.sub(POSTFIX, sprintf("try%02d", try_num + 1))
+#    Dir.mkdir next_dir
+#    FileUtils.cp( "#{@dir}/INCAR"  , "#{next_dir}/INCAR")
+#    FileUtils.cp( "#{@dir}/KPOINTS", "#{next_dir}/KPOINTS")
+#    FileUtils.cp( "#{@dir}/POTCAR" , "#{next_dir}/POTCAR")
+#    FileUtils.cp( "#{@dir}/CONTCAR", "#{next_dir}/POSCAR")
+#    initialize(next_dir)
+#  end
+#
+#  # Return number of electronic steps.
+#  def internal_steps
+#    return outcar[:electronic_steps] if outcar
+#    return 0
+#  end
+#
+#  # Return number of ionic steps.
+#  def external_steps
+#    return outcar[:ionic_steps] if outcar
+#    return 0
+#  end
+#
+#  # Return elapsed time.
+#  def elapsed_time
+#    return outcar[:elapsed_time] if outcar
+#    return 0.0
+#  end
+#
+#  # normal_ended? が false なら false。
+#  # normal_ended? が true のうち、
+#  # 結果を使って次に計算すべきなら true を、そうでなければ false を返す。
+#  #
+#  # 計算すべき、の条件はモードによって異なる。
+#  #   NSW = 0 もしくは NSW = 1 のとき、必ず false。
+#  #   - :single_point     モードならば、常に false。
+#  #   - :geom_opt_lattice モードならば、ionic step が 2 以上なら true。
+#  #   - :geom_opt_atoms   モードならば、ionic step が NSW と同じなら true。
+#  #
+#  def to_be_continued?
+#    begin
+#      outcar = Outcar.load_file("#{@dir}/OUTCAR")
+#    rescue Errno::ENOENT
+#      return false
+#    end
+#    ionic_steps = outcar[:ionic_steps]
+#    return false unless outcar[:normal_ended]
+#    return false if @incar["NSW"].to_i <= 1
+#    if @mode == :geom_opt_lattice
+#      return ionic_steps != 1
+#    elsif @mode == :geom_opt_atoms
+#      return ionic_steps == @incar["NSW"].to_i
+#    else
+#      return false
+#    end
+#  end
+#
--- test/test_vaspdir.rb
+++ test/test_vaspdir.rb
@@ -3,8 +3,13 @@
 
 $TEST = true
 
-class VaspDir
+require "rubygems"
+gem "comana"
+require "comana.rb"
+
+class VaspDir < Comana
   attr_reader :mode
+  public :finished?
 end
 
 require "test/unit"
@@ -17,55 +22,28 @@ require "vasputils/vaspdir.rb"
 
 class TC_VaspDir < Test::Unit::TestCase
 
-  GENERATED_FILES_VD00 = 
-  [
-    "test/vaspdir/not-yet-ISIF2/CHG",
-    "test/vaspdir/not-yet-ISIF2/CHGCAR",
-    "test/vaspdir/not-yet-ISIF2/CONTCAR",
-    "test/vaspdir/not-yet-ISIF2/DOSCAR",
-    "test/vaspdir/not-yet-ISIF2/EIGENVAL",
-    "test/vaspdir/not-yet-ISIF2/IBZKPT",
-    "test/vaspdir/not-yet-ISIF2/OSZICAR",
-    "test/vaspdir/not-yet-ISIF2/OUTCAR",
-    "test/vaspdir/not-yet-ISIF2/PCDAT",
-    "test/vaspdir/not-yet-ISIF2/WAVECAR",
-    "test/vaspdir/not-yet-ISIF2/XDATCAR",
-    "test/vaspdir/not-yet-ISIF2/machines",
-    "test/vaspdir/not-yet-ISIF2/vasprun.xml",
-    "test/vaspdir/not-yet-ISIF2/lock",
+  GENERATED_FILES_VD00 = [
+    "test/vaspdir/not-yet/CHG",
+    "test/vaspdir/not-yet/CHGCAR",
+    "test/vaspdir/not-yet/CONTCAR",
+    "test/vaspdir/not-yet/DOSCAR",
+    "test/vaspdir/not-yet/EIGENVAL",
+    "test/vaspdir/not-yet/IBZKPT",
+    "test/vaspdir/not-yet/OSZICAR",
+    "test/vaspdir/not-yet/OUTCAR",
+    "test/vaspdir/not-yet/PCDAT",
+    "test/vaspdir/not-yet/WAVECAR",
+    "test/vaspdir/not-yet/XDATCAR",
+    "test/vaspdir/not-yet/machines",
+    "test/vaspdir/not-yet/vasprun.xml",
+    "test/vaspdir/not-yet/lock",
   ]
 
   def setup
-    @vd00 = VaspDir.new("test/vaspdir/not-yet-ISIF2")
-    @vd01 = VaspDir.new("test/vaspdir/not-yet-ISIF3")
-    @vd02 = VaspDir.new("test/vaspdir/ISIF3-NSW100-OUTCAR-Iter1-INT")
-    @vd03 = VaspDir.new("test/vaspdir/ISIF2-NSW100-OUTCAR-Iter1")
-    @vd04 = VaspDir.new("test/vaspdir/ISIF3-NSW002-OUTCAR-Iter2")
-    @vd05 = VaspDir.new("test/vaspdir/ISIF3-NSW100-OUTCAR-Iter3")
-    @vd06 = VaspDir.new("test/vaspdir/PI")
-    @vd07 = VaspDir.new("test/vaspdir/lock")
-    @vd08 = VaspDir.new("test/vaspdir/lock-PI")
-    @vd09 = VaspDir.new("test/vaspdir/ISIF2-NSW100-OUTCAR-Iter3")
-    @vd10 = VaspDir.new("test/vaspdir/next-try00")
-    @vd11 = VaspDir.new("test/vaspdir/ISIF2-NSW000-OUTCAR-Iter1")
-    @vd12 = VaspDir.new("test/vaspdir/ISIF2-NSW001-OUTCAR-Iter1")
-    @vd13 = VaspDir.new("test/vaspdir/ISIF3-NSW000-OUTCAR-Iter1")
-    @vd14 = VaspDir.new("test/vaspdir/ISIF3-NSW001-OUTCAR-Iter1")
-    @vd15 = VaspDir.new("test/vaspdir/IBRION-1-NSW000-OUTCAR-Iter1")
-
-    GENERATED_FILES_VD00.each do |file|
-      FileUtils.rm file if File.exist? file
-    end
-  end
-
-  def teardown
-    ["test/vaspdir/next-try01/POSCAR" ,
-      "test/vaspdir/next-try01/POTCAR" ,
-      "test/vaspdir/next-try01/INCAR"  ,
-      "test/vaspdir/next-try01/KPOINTS"].each do |filename|
-      FileUtils.rm(filename) if File.exist?(filename)
-    end
-    Dir.rmdir("test/vaspdir/next-try01") if File.exist?("test/vaspdir/next-try01")
+    @vd00 = VaspDir.new("test/vaspdir/not-yet")
+    @vd01 = VaspDir.new("test/vaspdir/locked")
+    @vd02 = VaspDir.new("test/vaspdir/started")
+    @vd03 = VaspDir.new("test/vaspdir/finished")
 
     GENERATED_FILES_VD00.each do |file|
       FileUtils.rm file if File.exist? file
@@ -77,96 +55,24 @@ class TC_VaspDir < Test::Unit::TestCase
     assert_raise(VaspDir::InitializeError){VaspDir.new("test/vaspdir/lack-KPOINTS")}
     assert_raise(VaspDir::InitializeError){VaspDir.new("test/vaspdir/lack-POSCAR" )}
     assert_raise(VaspDir::InitializeError){VaspDir.new("test/vaspdir/lack-POTCAR" )}
-
-    assert_equal(:geom_opt_atoms,   @vd00.mode)
-    assert_equal(:geom_opt_lattice, @vd01.mode)
-    assert_equal(:geom_opt_lattice, @vd02.mode)
-    assert_equal(:geom_opt_atoms,   @vd03.mode)
   end
 
-  def test_name
-    assert_equal("test/vaspdir/not-yet-ISIF2"                , @vd00.name)
-    assert_equal("test/vaspdir/not-yet-ISIF3"                , @vd01.name)
-    assert_equal("test/vaspdir/ISIF3-NSW100-OUTCAR-Iter1-INT", @vd02.name)
-    assert_equal("test/vaspdir/ISIF2-NSW100-OUTCAR-Iter1"    , @vd03.name)
-    assert_equal("test/vaspdir/ISIF3-NSW002-OUTCAR-Iter2"    , @vd04.name)
-    assert_equal("test/vaspdir/ISIF3-NSW100-OUTCAR-Iter3"    , @vd05.name)
-    assert_equal("test/vaspdir/PI"                           , @vd06.name)
-    assert_equal("test/vaspdir/lock"                         , @vd07.name)
-    assert_equal("test/vaspdir/lock-PI"                      , @vd08.name)
-    assert_equal("test/vaspdir/ISIF2-NSW100-OUTCAR-Iter3"    , @vd09.name)
-    assert_equal("test/vaspdir/next-try00"                   , @vd10.name)
-    assert_equal("test/vaspdir/ISIF2-NSW000-OUTCAR-Iter1"    , @vd11.name)
-    assert_equal("test/vaspdir/ISIF2-NSW001-OUTCAR-Iter1"    , @vd12.name)
-    assert_equal("test/vaspdir/ISIF3-NSW000-OUTCAR-Iter1"    , @vd13.name)
-    assert_equal("test/vaspdir/ISIF3-NSW001-OUTCAR-Iter1"    , @vd14.name)
-    assert_equal("test/vaspdir/IBRION-1-NSW000-OUTCAR-Iter1" , @vd15.name)
-  end
-
-  def test_started?
-    assert_equal(false, @vd00.started?)
-    assert_equal(false, @vd01.started?)
-    assert_equal(true , @vd02.started?)
-    assert_equal(true , @vd03.started?)
-    assert_equal(true , @vd04.started?)
-    assert_equal(true , @vd05.started?)
-    assert_equal(false, @vd06.started?)
-    assert_equal(true , @vd07.started?)
-    assert_equal(true , @vd08.started?)
-    assert_equal(true , @vd09.started?)
-    assert_equal(true , @vd10.started?)
-    assert_equal(true , @vd11.started?)
-    assert_equal(true , @vd12.started?)
-    assert_equal(true , @vd13.started?)
-    assert_equal(true , @vd14.started?)
-    assert_equal(true , @vd15.started?)
-  end
-
-  def test_normal_ended?
-    assert_equal(false, @vd00.normal_ended?)
-    assert_equal(false, @vd01.normal_ended?)
-    assert_equal(false, @vd02.normal_ended?)
-    assert_equal(true , @vd03.normal_ended?)
-    assert_equal(true , @vd04.normal_ended?)
-    assert_equal(true , @vd05.normal_ended?)
-    assert_equal(false, @vd06.normal_ended?)
-    assert_equal(false, @vd07.normal_ended?)
-    assert_equal(false, @vd08.normal_ended?)
-    assert_equal(true , @vd09.normal_ended?)
-  end
-
-  def test_to_be_continued?
-    assert_equal(false, @vd00.to_be_continued?)
-    assert_equal(false, @vd01.to_be_continued?)
-    assert_equal(false, @vd02.to_be_continued?)
-    assert_equal(false, @vd03.to_be_continued?)
-    assert_equal(true , @vd04.to_be_continued?)
-    assert_equal(true , @vd05.to_be_continued?)
-    assert_equal(false, @vd06.to_be_continued?)
-    assert_equal(false, @vd07.to_be_continued?)
-    assert_equal(false, @vd08.to_be_continued?)
-    assert_equal(false, @vd09.to_be_continued?)
-    assert_equal(true , @vd10.to_be_continued?)
-    assert_equal(false, @vd11.to_be_continued?)
-    assert_equal(false, @vd12.to_be_continued?)
-    assert_equal(false, @vd13.to_be_continued?)
-    assert_equal(false, @vd14.to_be_continued?)
-    assert_equal(false, @vd15.to_be_continued?)
+  def test_finished?
+    assert_equal(false, @vd00.finished?)
+    assert_equal(false, @vd01.finished?)
+    assert_equal(false, @vd02.finished?)
+    assert_equal(true , @vd03.finished?)
   end
 
   def test_calculate
-    assert_raise(VaspDir::LockedError){@vd07.calculate}
-    assert_raise(VaspDir::LockedError){@vd08.calculate}
-
-    #
     assert_nothing_raised{@vd00.calculate}
     GENERATED_FILES_VD00.each do |file|
       assert(FileTest.exist?(file), "#{file} not found.")
     end
-    assert(FileTest.exist? "test/vaspdir/not-yet-ISIF2/INCAR")
-    assert(FileTest.exist? "test/vaspdir/not-yet-ISIF2/KPOINTS")
-    assert(FileTest.exist? "test/vaspdir/not-yet-ISIF2/POSCAR")
-    assert(FileTest.exist? "test/vaspdir/not-yet-ISIF2/POTCAR")
+    assert(FileTest.exist? "test/vaspdir/not-yet/INCAR")
+    assert(FileTest.exist? "test/vaspdir/not-yet/KPOINTS")
+    assert(FileTest.exist? "test/vaspdir/not-yet/POSCAR")
+    assert(FileTest.exist? "test/vaspdir/not-yet/POTCAR")
     #
     io = File.open("test/vaspdir/not-yet-ISIF2/lock", "r")
     lines = io.readlines
@@ -178,59 +84,13 @@ class TC_VaspDir < Test::Unit::TestCase
     # あとかたづけは teardown にまかせる。
   end
 
-  def test_next
-    assert_raise(VaspDir::NotEndedError){@vd00.next}
-    assert_raise(VaspDir::NotEndedError){@vd01.next}
-    assert_raise(VaspDir::NotEndedError){@vd02.next}
-    assert_raise(VaspDir::ConvergedError){@vd03.next}
-    assert_raise(VaspDir::NotEndedError){@vd06.next}
-    assert_raise(VaspDir::NotEndedError){@vd07.next}
-    assert_raise(VaspDir::NotEndedError){@vd08.next}
-    assert_raise(VaspDir::ConvergedError){@vd09.next}
-
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try00"))
-    assert_equal(false, FileTest.exist?("test/vaspdir/next-try01"))
-    @vd10.next
-    assert_equal("test/vaspdir/next-try01", @vd10.name)
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try00"))
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try01"))
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try01/POSCAR"))
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try01/POTCAR"))
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try01/INCAR"))
-    assert_equal(true , FileTest.exist?("test/vaspdir/next-try01/KPOINTS"))
-    assert_equal(4 , Dir.glob("test/vaspdir/next-try01/*").size)
-    #
-    poscar_00  = File.open("test/vaspdir/next-try00/POSCAR" ).readlines
-    contcar_00 = File.open("test/vaspdir/next-try00/CONTCAR").readlines
-    poscar_01  = File.open("test/vaspdir/next-try01/POSCAR" ).readlines
-    assert_equal(false, poscar_00  == contcar_00)
-    assert_equal(true,  contcar_00 == poscar_01)
-    assert_equal(false, poscar_01  == poscar_00)
-    # あとかたづけは teardown にまかせる。
-  end
-
-  def test_teardown
-    # NO TEST
-  end
-
-  def test_internal_steps
-    assert_equal(18, @vd10.internal_steps)
-  end
-
-  def test_external_steps
-    assert_equal(2, @vd10.external_steps)
-  end
-
-  def test_elapsed_time
-    assert_in_delta(126.383, @vd10.elapsed_time)
-  end
-
   def test_outcar
-    assert_equal("test/vaspdir/next-try00/OUTCAR", @vd10.outcar[:name])
+    assert_equal("test/vaspdir/started/OUTCAR", @vd02.outcar[:name])
+    assert_equal("test/vaspdir/finished/OUTCAR", @vd03.outcar[:name])
   end
 
   def test_contcar
-    t = @vd10.contcar
+    t = @vd00.contcar
     assert_equal(Cell, t.class)
     assert_in_delta(3.8678456093562040, t.axes[2][2])
     
@@ -238,12 +98,12 @@ class TC_VaspDir < Test::Unit::TestCase
   end
 
   def test_incar
-    t = @vd10.incar
+    t = @vd00.incar
     assert_equal("400", t["ENCUT"])
   end
 
   def test_kpoints
-    t = @vd10.kpoints
+    t = @vd00.kpoints
     assert_equal("Automatic mesh", t[:comment])
   end
 
