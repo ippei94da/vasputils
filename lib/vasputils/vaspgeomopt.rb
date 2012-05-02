7eebd464848f10cf3134283640c3a2f3aefb6ac6
ippei <ippei@As1.(none)>
2012-05-02 10:31:56 +0900

    small revision

--- lib/vasputils/vaspgeomopt.rb
+++ lib/vasputils/vaspgeomopt.rb
@@ -9,23 +9,25 @@ require "comana"
 #
 #
 class VaspGeomOpt < Comana
-  #
-  def initialize()
-  end
 
-  def send_command
+  def calculate
+    最新の状態を確認する。
+
+    配下のディレクトリをすべて確認する。
+      VaspDir 以外のものは無視。
+      VaspDir のうち、名前が最後のものだけ注目。それ以外は無視。
+        <=> が定義できるような命名規則がある筈で、
+        それは大抵単純な昇順ソートでいけるようなものだろう。
+      注目した VaspDir が yet なら実行し、続ける。
+      yet 以外なら例外。
+
+    VaspDir になっているか。
+
+
     raise NotImplementedError, "#{self.class}::send_command need to be redefined"
+
   end
 
-  def set_parameters
-    raise NotImplementedError, "#{self.class}::set_parameters need to be redefined"
-    @lockdir    = "vaspgeomopt_lock"
-    @alive_time = 3600
-    @outfiles   = [] # Files only to output should be indicated.
-  end
-
-  # Return true if the condition is satisfied.
-  # E.g., when calculation output contains orthodox ending sequences.
   def finished?
     raise NotImplementedError, "#{self.class}::finished? need to be redefined"
   end
