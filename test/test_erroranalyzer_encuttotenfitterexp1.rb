f9292b9a5f935f88d635984a838ec4ebcdf66860
ippei94da <ippei94da@gmail.com>
2013-02-14 21:04:42 +0900

    small revision

--- test/test_erroranalyzer_encuttotenfitterexp1.rb
+++ test/test_erroranalyzer_encuttotenfitterexp1.rb
@@ -8,7 +8,8 @@ class TC_EncutTotenFitterExp1 < Test::Unit::TestCase
   TOLERANCE = 1.0E-10
 
   def setup
-    data_pairs = [
+    data_pairs =
+    [
       [ 0.0, 104.0],
       [ 1.0, 102.0],
       [ 2.0, 101.0],
@@ -26,15 +27,17 @@ class TC_EncutTotenFitterExp1 < Test::Unit::TestCase
   end
 
   def test_fit
+    corrects = [4.0, - Math::log(2.0)]
     results = @etf00.fit
     assert_equal(   2, results.size)
-    assert_in_delta( 4.0, results[0], TOLERANCE)
-    assert_in_delta( - Math::log(2.0), results[1], TOLERANCE)
+    assert_in_delta(corrects[0] , results[0], TOLERANCE)
+    assert_in_delta(corrects[1] , results[1], TOLERANCE)
 
+    corrects = [4.0, - Math::log(2.0)]
     results = @etf01.fit
     assert_equal(   2, results.size)
-    assert_in_delta( 4.0, results[0], TOLERANCE)
-    assert_in_delta( - Math::log(2.0), results[1], TOLERANCE)
+    assert_in_delta(corrects[0] , results[0], TOLERANCE)
+    assert_in_delta(corrects[1] , results[1], TOLERANCE)
   end
 
   def test_expected_errors
