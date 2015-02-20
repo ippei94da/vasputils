f46102857479c905fdc45daf8e6fe21e3c2f9c04
ippei94da <ippei94da@gmail.com>
2015-02-20 10:31:37 +0900

    daily change

---
+++ test/test_vaspcelloptimizer.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+require "helper"
+#require "test/unit"
+#require "pkg/klass.rb"
+
+class TC_CellOptimizer < Test::Unit::TestCase
+    def setup
+        @c00 = VaspUtils::CellOptimizer.new
+    end
+
+end
+
