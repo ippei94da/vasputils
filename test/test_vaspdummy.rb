1c3c498c961300316876e458055a73f58d3d67c4
ippei94da <ippei94da@gmail.com>
2015-02-22 17:25:45 +0900

    daily change

---
+++ test/test_vaspdummy.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+require "helper"
+#require "test/unit"
+#require "pkg/klass.rb"
+
+class TC_VaspDir
+
+class TC_VaspDummy < Test::Unit::TestCase
+    def setup
+        @vd00 = VaspUtils::VaspDir.new
+    end
+
+end
+
---
+++ test/vaspdummy.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+#
+#
+#
+class VaspUtils::VaspDir < Comana::ComputationManager
+    #
+    def initialize()
+    end
+end
+
