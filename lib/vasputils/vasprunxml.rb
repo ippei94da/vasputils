69a65339a9a767a6ec610612805a12070571f424
ippei94da <ippei94da@gmail.com>
2015-07-23 22:51:36 +0900

    daily change

--- lib/vasputils/vasprunxml.rb
+++ lib/vasputils/vasprunxml.rb
@@ -27,5 +27,36 @@ class VaspUtils::VasprunXml
     end
     items[-3..-1]
   end
+
+  def bases
+    results = @data.xpath("/modeling/calculation").map do |c|
+      c.xpath("./structure/crystal/varray[@name='basis']/v").map do |axis|
+        axis.text.strip.split.map {|i| i.to_f}
+      end
+    end
+  end
+
+  def positions_list
+    @data.xpath("/modeling/calculation").map do |c|
+      c.xpath("./structure/varray[@name='positions']/v").map do |pos|
+        pos.text.strip.split.map {|i| i.to_f}
+      end
+    end
+    #t = @data.xpath("/modeling/calculation/structure/crystal/varray[@name='basis']/v").map { |i| pp i.text.strip.split.map{|j| j.to_f}}
+    #pp t.methods.sort
+  end
+
+  def nums_elements
+    @data.xpath("/modeling/atominfo/array[@name='atomtypes']/set/rc").map do |elem|
+      elem.xpath('./c').children[0].text.to_i
+    end
+  end
+
+  def elements
+    @data.xpath("/modeling/atominfo/array[@name='atomtypes']/set/rc").map do |elem|
+      elem.xpath('./c').children[1].text
+    end
+  end
+
 end
 
--- test/test_vasprunxml.rb
+++ test/test_vasprunxml.rb
@@ -6,26 +6,66 @@ require "helper"
 #require "pkg/klass.rb"
 
 class TC_VasprunXml < Test::Unit::TestCase
-    def setup
-        #@v00 = VaspUtils::VasprunXml.new
-        @v00 = VaspUtils::VasprunXml.load_file('test/vasprunxml/singlepoint.xml')
-        @v01 = VaspUtils::VasprunXml.load_file('test/vasprunxml/geomopt.xml')
-    end
+  def setup
+    #@v00 = VaspUtils::VasprunXml.new
+    @v00 = VaspUtils::VasprunXml.load_file('test/vasprunxml/singlepoint.xml')
+    @v01 = VaspUtils::VasprunXml.load_file('test/vasprunxml/geomopt.xml')
+  end
 
-    def test_load_file
-        correct = [
-            [ 327.28361242,   -291.24674632,      0.00000000],
-            [-291.24674632,    327.28361242,      0.00000000],
-            [   0.00000000,      0.00000000,     28.53476763],
-        ]
-        assert_equal(correct, @v00.stress)
+  def test_load_file
+    correct = [
+      [ 327.28361242,   -291.24674632,      0.00000000],
+      [-291.24674632,    327.28361242,      0.00000000],
+      [   0.00000000,      0.00000000,     28.53476763],
+    ]
+    assert_equal(correct, @v00.stress)
 
-        correct = [
-            [-1.90067153,     -0.00000000,     -0.00000000],
-            [ 0.00000000,     -1.90067153,      0.00000000],
-            [-0.00000000,      0.00000000,     -1.90067153],
-        ]
-        assert_equal(correct, @v01.stress)
-    end
+    correct = [
+      [-1.90067153,     -0.00000000,     -0.00000000],
+      [ 0.00000000,     -1.90067153,      0.00000000],
+      [-0.00000000,      0.00000000,     -1.90067153],
+    ]
+    assert_equal(correct, @v01.stress)
+  end
+
+  def test_bases
+    correct = [
+      [ [-1.69543387, -1.69543387, 1.69543387],
+        [-1.69543387, 1.69543387, -1.69543387],
+        [1.69543387, -1.69543387, -1.69543387]],
+      [ [-1.62179317, -1.62179317, 1.62179317],
+        [-1.62179317, 1.62179317, -1.62179317],
+        [1.62179317, -1.62179317, -1.62179317]],
+      [ [-1.51067861, -1.51067861, 1.51067861],
+        [-1.51067861, 1.51067861, -1.51067861],
+        [1.51067861, -1.51067861, -1.51067861]],
+      [ [-1.4842561, -1.4842561, 1.4842561],
+        [-1.4842561, 1.4842561, -1.4842561],
+        [1.4842561, -1.4842561, -1.4842561]],
+      [ [-1.49234629, -1.49234629, 1.49234629],
+        [-1.49234629, 1.49234629, -1.49234629],
+        [1.49234629, -1.49234629, -1.49234629]]
+    ]
+    assert_equal(correct, @v01.bases)
+  end
+
+  def test_positions_list
+    correct = [
+      [[0.0, 0.0, 0.0]],
+      [[0.0, 0.0, 0.0]],
+      [[0.0, 0.0, 0.0]],
+      [[0.0, 0.0, 0.0]],
+      [[0.0, 0.0, 0.0]]
+    ]
+    assert_equal(correct, @v01.positions_list)
+  end
+
+  def test_nums_elements
+    assert_equal([1], @v01.nums_elements)
+  end
+
+  def test_elements
+    assert_equal(['Li'], @v01.elements)
+  end
 end
 
