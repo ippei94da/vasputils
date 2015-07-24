23f25739e6835363c1f80979876922476556eedc
ippei94da <ippei94da@gmail.com>
2015-07-24 22:54:32 +0900

    daily change

--- example/povray/4angle/make.zsh
+++ example/povray/4angle/make.zsh
@@ -1,10 +1,10 @@
 #! /usr/bin/env zsh
 
 poscar povray POSCAR > cell.inc
-povray main-x.pov
-povray main-y.pov
-povray main-z.pov
-povray main-w.pov
+povray main-x.pov -display
+povray main-y.pov -display
+povray main-z.pov -display
+povray main-w.pov -display
 
 convert +append main-x.png main-y.png append2-xy.png
 convert +append main-z.png main-w.png append2-zw.png
